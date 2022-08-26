e5ac557ffdde15fe78081e72fafe4e497e9b9285
DESKTOP-B65V3QH\Admin <nithesh.k@honeywell.com>
2022-08-26 18:33:00 +0530

    commit1

--- ReadAppSettingJSON/Controllers/DemoController.cs
+++ ReadAppSettingJSON/Controllers/DemoController.cs
@@ -18,12 +18,12 @@ namespace ReadAppSettingJSON.Controllers
         [Route("~/")]
         public IActionResult Index()
         {
-            var config1 = configuration["DefaultLevel:Error"];
-            Debug.WriteLine("Error", config1);
-            var config2 = configuration["DefaultLevel:Debug"];
-            Debug.WriteLine("Debug", config2);
-            var config3 = configuration["DefaultLevel:Warning"];
-            Debug.WriteLine("Warning", config3);
+            //var config1 = configuration["DefaultLevel:Error"];
+            //Debug.WriteLine("Error", config1);
+            //var config2 = configuration["DefaultLevel:Debug"];
+            //Debug.WriteLine("Debug", config2);
+            //var config3 = configuration["DefaultLevel:Warning"];
+            //Debug.WriteLine("Warning", config3);
             return View();
         }
     }
